ac936f416d1cec0ecf812a452fabe83f4de30139
BassamHajar <b2dndr1@gmail.com>
2020-04-15 21:13:42 +0200

    did: request & body validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('users route'))
+router.post('/', [check('name', 'name is required').not().isEmpty(), check('email', 'insert a valid email').normalizeEmail().isEmail(), check('password', 'please insert 6 characters min for password').isLength({ min: 6 })], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    res.send('users route')
+})
 
 module.exports = router;
