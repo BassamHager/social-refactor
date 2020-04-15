cdf0e44246469e794f3da4744bee966480480654
BassamHajar <b2dndr1@gmail.com>
2020-04-15 19:36:53 +0200

    npm i packages, basic server.js file

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API is running...'))
+
+app.listen(PORT, () => console.log(`This app started on port ${PORT}`))
