57dbb371cd6651a0117cc3562eadb9a0040aee59
BassamHajar <b2dndr1@gmail.com>
2020-04-15 20:15:03 +0200

    added: routes/api folders. auth, posts, profile, users js files. did: define routes

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db')
 
-// connect db
-connectDB()
-
-
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// connect db
+connectDB()
+
+// define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 app.get('/', (req, res) => res.send('API is running...'))
 
