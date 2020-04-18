57bdc3b3d336163e00bf55f5edce2e1602d3e8b9
BassamHajar <b2dndr1@gmail.com>
2020-04-18 16:39:14 +0200

    added: Post.js . did: Post shcema, add post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,44 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
+const { check, validationResult } = require("express-validator");
+const request = require("request");
+const config = require("config");
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('posts route'))
+// @route   POST api/posts
+// @desc    Add a post
+// @access  Private
+router.post(
+  "/",
+  [auth, check("text", "Text is required!")],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.sendStatus(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await (await User.findById(req.user.id)).isSelected(
+        "-password"
+      );
+
+      const newPost = new Post({
+        text: req.body.text,
+        user: req.user.id,
+        name: user.name,
+        avatar: user.avatar,
+      });
+
+      const post = await newPost.save();
+      return res.json(post);
+    } catch (err) {
+      console.log(err.message);
+      return res.sendStatus(500).json("Server error!");
+    }
+  }
+);
 
 module.exports = router;
