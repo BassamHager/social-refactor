376525fd0eb5a3f9bb19dc30ea89f5f82f569dd0
BassamHajar <b2dndr1@gmail.com>
2020-04-19 08:08:55 +0200

    added: auth folder, Register Login jsx files. did: react router setup, connecting Navbar & Landing links

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./componentes/layout/Navbar";
 import Landing from "./componentes/layout/Landing";
+import Register from "./componentes/auth/Register";
+import Login from "./componentes/auth/Login";
 
 const App = () => (
-  <Fragment>
+  <Router>
     <Navbar />
-    <Landing />
-  </Fragment>
+    <Route exact path="/" component={Landing} />
+    <section className="container">
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </section>
+  </Router>
 );
 
 export default App;
