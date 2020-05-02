2b0c8feab828317790d2be578b1ff5664b25a2d0
BassamHajar <b2dndr1@gmail.com>
2020-05-02 07:40:00 +0200

    did: comment to add Redirect to /auth in App.js router switch, to shift / into the switch & adding privacy into developers route in Navbar.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/pages/Landing";
@@ -34,9 +39,11 @@ const App = () => {
           <Route exact path="/" component={Landing} />
           <section className="container">
             <Switch>
+              {/* <Route exact path="/" component={Landing} /> */}
               <Route exact path="/auth" component={Auth} />
               <Route exact path="/dashboard" component={Dashboard} />
               <Route exact path="/profile" component={Profile} />
+              {/* <Redirect to="auth" /> */}
             </Switch>
           </section>
         </AlertContext.Provider>
--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,15 +1,12 @@
-import React, { useContext, useState, Fragment } from "react";
+import React, { useContext, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth-context";
 import Alert from "../layout/Alert";
 import { AlertContext } from "../context/alert-context";
-import Dashboard from "./pages/Dashboard";
 
 const Navbar = () => {
   const { setAlert } = useContext(AlertContext);
-  const { token, setIsToLoginMode, logout, login, isToLoginMode } = useContext(
-    AuthContext
-  );
+  const { token, setIsToLoginMode, logout, login } = useContext(AuthContext);
 
   const handleLogging = () => {
     if (token) {
@@ -25,14 +22,14 @@ const Navbar = () => {
     <Fragment>
       <nav className="navbar bg-dark">
         <h1>
-          <Link to="/">
-            <i className="fas fa-code"></i> Social_Dev_Connector
-          </Link>
+          <Link to="/">Social_Dev_Connector</Link>
         </h1>
         <ul>
-          <li>
-            <Link to="/">Developers</Link>
-          </li>
+          {token && (
+            <li>
+              <Link to="/developers">Developers</Link>
+            </li>
+          )}
           {!token && (
             <li>
               <Link to="/auth" onClick={() => setIsToLoginMode(false)}>
